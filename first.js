135ebe24162143b4089810dedc3574a657669f64
Shruti Patel <76270372+shrutijpatel@users.noreply.github.com>
2022-01-25 17:10:46 +0530

    Update first.js

--- first.js
+++ first.js
@@ -24,12 +24,13 @@ const getHotel = () => {
     while(hotels.length>0){
         hotels.remove(0)
     }
-    fetch(`${hotelUrl}${cityId}`)
+    fetch(`${hotelUrl}?city=${cityId}`)
     .then((res) => res.json())
     .then((data) => {
+        console.log(data[i])
         for(i=0;i<data.length;i++){
             let element = document.createElement('option')
-            let text = document.createTextNode(`${data[i].name} | ${data[i].city_name}`)
+            let text = document.createTextNode(data[i].name)
             element.appendChild(text)
             document.getElementById('hotels').appendChild(element)
         }
