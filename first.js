45a45f0a16b8417279a13d522764dcec220529f0
shrutijpatel <shruti.j.patel1999@gmail.com>
2022-02-11 21:13:02 +0530

    Work

--- first.js
+++ first.js
@@ -1,35 +1,33 @@
 
-let url = "http://localhost:1255/city"
-let hotelUrl = "https://developerfunnel.herokuapp.com/hotels?city="
+let url = "http://localhost:1298/city";
+const hotelUrl = "http://localhost:1298/hotel";
 
 function getCity(){
     fetch(url)
     .then((res) => res.json())
     .then((data) => {
         for(i=0;i<data.length;i++){
-            let element = document.createElement('option')  //<option></option>
-            let text = document.createTextNode(data[i].city_name) // Delhi
-            element.appendChild(text) //<option>Delhi</option>
-            element.value = data[i]._id //<option value="1">Delhi</option>
+            let element = document.createElement('option')
+            let text = document.createTextNode(data[i].city_name)
+            element.appendChild(text)
+            element.value = data[i]._id
             document.getElementById('city').appendChild(element)
-            /*<select>
-                <option value="1">Delhi</option>
-            </select>*/
         }
     })
 }
 
-const getHotel = () => {
+const getHotels = () => {
     const cityId = document.getElementById('city').value;
     while(hotels.length>0){
         hotels.remove(0)
     }
-    fetch(`${hotelUrl}${cityId}`)
+    fetch(`${hotelUrl}?city=${cityId}`)
     .then((res) => res.json())
     .then((data) => {
+        console.log(data[i])
         for(i=0;i<data.length;i++){
             let element = document.createElement('option')
-            let text = document.createTextNode(`${data[i].name} | ${data[i].city_name}`)
+            let text = document.createTextNode(data[i].name)
             element.appendChild(text)
             document.getElementById('hotels').appendChild(element)
         }
